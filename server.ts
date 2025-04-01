2bb239a5b5c8cef65a90652c8669f7113be4594b
Pilks-pixel <peterpilkington@hotmail.com>
2025-04-01 22:49:40 +0100

    file set up

---
+++ server.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import { createServer } from 'node:http'
+
+var app = express();
+var server = createServer(app);
+var PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+server.listen(PORT, function() {
+  console.log(`listening on ${PORT}`);
+});
+
