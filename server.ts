f658fd5fa4f6e5aaa96328840377da6237d814eb
Pilks-pixel <peterpilkington@hotmail.com>
2025-04-02 22:27:47 +0100

    added logic to dynamically render forks per cpu

--- server.ts
+++ server.ts
@@ -1,26 +1,29 @@
 import express from "express";
 import { createServer } from "node:http";
 import cluster from "node:cluster";
+import { cpus } from "node:os";
 
 var app = express();
 var server = createServer(app);
 var PORT = process.env.PORT || 3000;
+var numCPUs = cpus().length;
 
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send(`Hello World! ${process.pid}`);
 });
 
 app.get("/timer", function (req, res) {
   delay(5000);
-  res.send("ding ding ding");
+  res.send(`ding ding ding ${process.pid}`);
 });
 
 if (cluster.isPrimary) {
   console.log(`Master ${process.pid} is running`);
-  cluster.fork();
-  cluster.fork();
+  for (let i = 0; i < numCPUs - 1; i++) {
+    cluster.fork();
+  }
 } else {
   console.log(`worker ${process.pid} started`);
   server.listen(PORT, function () {
