e49e670cbaa93bc1b82828d15194b0f27b0021ab
Pilks-pixel <peterpilkington@hotmail.com>
2025-04-02 07:25:11 +0100

    set up cluster and workers

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { createServer } from "node:http";
+import cluster from "node:cluster";
 
 var app = express();
 var server = createServer(app);
@@ -16,9 +17,16 @@ app.get("/timer", function (req, res) {
   res.send("ding ding ding");
 });
 
-server.listen(PORT, function () {
-  console.log(`listening on ${PORT}`);
-});
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log(`worker ${process.pid} started`);
+  server.listen(PORT, function () {
+    console.log(`listening on ${PORT}`);
+  });
+}
 
 function delay(duration: number) {
   const startTime = Date.now();
