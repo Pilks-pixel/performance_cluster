49466f8e20477280ccf24a1745c793c3b57d1a25
Pilks-pixel <peterpilkington@hotmail.com>
2025-04-02 07:02:04 +0100

    delay route & function added

--- server.ts
+++ server.ts
@@ -7,10 +7,22 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.send("Hello World!");
+});
+
+app.get("/timer", function (req, res) {
+  delay(5000);
+  res.send("ding ding ding");
 });
 
 server.listen(PORT, function () {
   console.log(`listening on ${PORT}`);
 });
+
+function delay(duration: number) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {
+    //event loop is blocked...
+  }
+}
